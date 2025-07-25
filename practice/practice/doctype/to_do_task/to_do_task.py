ed15fa482c68d1d7c41297b144e97acdff98daa6
Ravendra Singh <ravendra24@navgurukul.org>
2025-07-25 15:42:18 +0530

    create Frappe Rest API

--- practice/practice/doctype/to_do_task/to_do_task.py
+++ practice/practice/doctype/to_do_task/to_do_task.py
@@ -3,21 +3,26 @@
 
 
 import frappe
-from frappe.model.document import Document
-from frappe.utils import today, getdate
+# from frappe.model.document import Document
+# from frappe.utils import today, getdate
 
 
-class ToDoTask(Document):
-    def validate(self):
-        if getdate(self.due_date) >= getdate(today()):
-            pass
-        else:
-            frappe.throw("Due date should be today or a future date.")
+# class ToDoTask(Document):
+#     def validate(self):
+#         if getdate(self.due_date) >= getdate(today()):
+#             pass
+#         else:
+#             frappe.throw("Due date should be today or a future date.")
 
-@frappe.whitelist()
-def mark_completed(docname):
-    doc = frappe.get_doc("To-Do Task", docname)
-    doc.status = "Completed"
-    doc.save()
-    return "Marked as Completed"
+# @frappe.whitelist()
+# def mark_completed(docname):
+#     doc = frappe.get_doc("To-Do Task", docname)
+#     doc.status = "Completed"
+#     doc.save()
+#     return "Marked as Completed"
 
+
+@frappe.whitelist(allow_guest=True)
+def get_tasks():
+    tasks = frappe.get_all("To-Do Task", fields=["name", "tittle", "status"])
+    return tasks
